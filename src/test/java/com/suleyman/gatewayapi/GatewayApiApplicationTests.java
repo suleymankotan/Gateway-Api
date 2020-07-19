0a4ca002ef3ef6ead24bb2b05b970837b38b9083
suleymankotan <suleymankotan@gmail.com>
2020-07-19 03:00:29 +0300

    initial commit

---
+++ src/test/java/com/suleyman/gatewayapi/GatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suleyman.gatewayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
